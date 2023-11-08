0fd90758878ac78921e324632c1687205e2f96a0
José Cabral <cabraljose1210@gmail.com>
2023-11-08 20:51:45 -0300

    Pantalla principal creada

---
+++ bibliotecaApp.py
@@ -0,0 +1,7 @@
+from boundaries.pantallaPpal import PantallaPrincipal
+import tkinter as tk
+
+if __name__ == "__main__":
+    ventana = tk.Tk()
+    app = PantallaPrincipal(ventana)
+    ventana.mainloop()
--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+#====================================================================================================================
+#====================================================================================================================
+# DATA
 path_bd = "./database/biblioteca.sqlite"
+
+#====================================================================================================================
+#====================================================================================================================
+# TKINTER
+png_socios = "./boundaries/icons/socios.png"
+png_reportes = "./boundaries/icons/reportes.png"
+png_libros = "./boundaries/icons/libros.png"
+png_extravios = "./boundaries/icons/extravios.png"
+png_prestamos = "./boundaries/icons/prestamos.png"
+png_salir = "./boundaries/icons/salir.png"
